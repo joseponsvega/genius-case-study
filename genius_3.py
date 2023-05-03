ecb0f2e2dcacaa07f4f7fc9ea1cc09223991be6a
Jose Pons Vega <jose@protonmail.com>
2023-05-02 22:10:42 -0700

    David Kim answer

--- genius_3.py
+++ genius_3.py
@@ -10,11 +10,11 @@ from langchain.tools import tool
 
 @tool
 def search_csv(query: str) -> str:
-    """Answers questions about a csv file. The format is the name of the csv file, then a comma, then the question. For example: 'data.csv,what is the csv about?'"""
+    """Answers questions about a csv file. The format is the name of the csv file, then a comma, then the question. For example: 'data.csv,what is the csv about?'."""
 
     results = query.split(",")
 
-    agent = create_csv_agent(OpenAI(temperature=0), results[0], verbose=True)
+    agent = create_csv_agent(OpenAI(temperature=0), results[0].replace("'", ""), verbose=True)
 
     response = agent.run(results[1])
 
@@ -22,15 +22,15 @@ def search_csv(query: str) -> str:
 
 @tool
 def open_text_file(filename: str) -> str:
-    """Opens the text file and returns the contents."""
+    """Opens the text file and returns the contents. Don't forget to use the structure of Action, Action Input, etc."""
 
-    with open(filename) as infile:
+    with open(filename.replace("\n", "").replace("'", "")) as infile:
         return infile.read()
 
 
-llm = OpenAI(temperature=0)
+llm = OpenAI(temperature=0, model_name="gpt-4")
 
-tools = load_tools(["serpapi", "llm-math"], llm=llm) + [search_csv, open_text_file]
+tools = load_tools(["llm-math"], llm=llm) + [search_csv, open_text_file]
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 
@@ -56,7 +56,9 @@ toolkit = FileManagementToolkit(root_dir=str(working_directory.name)) # If you d
 
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, memory=memory)
 
-agent.run("You have the following files at your disposal: Grades.csv, Professors.csv, Syllabus.txt and Weekly Schedule.txt. Read the csv files then translate them to plain text descriptions. Then use that and the text files to figure out which professors teach mathematics.")
+question = "Which of the Mathematics professors are male?"
+
+agent.run(f"You have the following files at your disposal: Grades.csv, Professors.csv, Syllabus.txt and Weekly Schedule.txt. First, summarize each file and what information it contains. Then using those files and knowing the information within each one, answer the following question: {question}  Don't forget to use the structure of Action, Action Input, etc.")
 
 # agent = create_csv_agent(OpenAI(temperature=0), 'Grades.csv', verbose=True)
 #
