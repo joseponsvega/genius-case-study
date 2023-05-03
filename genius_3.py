7434ebf34453839b72b9eec5e78742c7ba0bf115
Jose Pons Vega <jose@protonmail.com>
2023-05-02 22:40:31 -0700

    Able to answer avg age of math teachers

--- genius_3.py
+++ genius_3.py
@@ -1,12 +1,11 @@
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
 from langchain.agents import AgentType
-from langchain.llms import OpenAI
 from langchain.agents import create_csv_agent
-from typing import Optional, Type
-
+from langchain.agents import initialize_agent
+from langchain.agents import load_tools
+from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.tools import tool
+
 
 @tool
 def search_csv(query: str) -> str:
@@ -34,32 +33,13 @@ tools = load_tools(["llm-math"], llm=llm) + [search_csv, open_text_file]
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 
-from langchain.tools.file_management import (
-    ReadFileTool,
-    CopyFileTool,
-    DeleteFileTool,
-    MoveFileTool,
-    WriteFileTool,
-    ListDirectoryTool,
-)
-from langchain.agents.agent_toolkits import FileManagementToolkit
 from tempfile import TemporaryDirectory
 
 # We'll make a temporary directory to avoid clutter
 working_directory = TemporaryDirectory()
 
-toolkit = FileManagementToolkit(root_dir=str(working_directory.name)) # If you don't provide a root_dir, operations will default to the current working directory
-
-
-# tools += toolkit.get_tools()
-
-
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, memory=memory)
 
-question = "What days and periods is the subject with the lowest average grade taught?"
+question = "What is the average grade for the subject taught in 1st period on Tuesday?"
 
-agent.run(f"You have the following files at your disposal: Grades.csv, Professors.csv, Syllabus.txt and Weekly Schedule.txt. First, summarize each file and what information it contains. Then using those files and knowing the information within each one, answer the following question: {question}  Don't forget to use the structure of Action, Action Input, etc.")
-
-# agent = create_csv_agent(OpenAI(temperature=0), 'Grades.csv', verbose=True)
-#
-# agent.run("What is the average grade in Economics?")
+agent.run(f"You have the following files at your disposal: Grades.csv, Professors.csv, Syllabus.txt and Weekly Schedule.txt. First, summarize each file and what information it contains. Then using those files and knowing the information within each one, answer the following question: {question} Remember to use the structure of Action, Action Input, etc.")
